501a1646da030656c1b1c570d1c825017205f55a
Ashwin Sathawane <ashusath@gmail.com>
2018-08-17 17:17:44 +0530

    Initial commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,137 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const APP_NAME = "Location Starter"
+const messages = {
+  NOTIFY_MISSING_PERMISSIONS: 'Please enable device location permissions in the Amazon Alexa app.',
+  NO_ADDRESS: 'It looks like you don\'t have an address set. You can set your address from the companion app.',
+  ERROR: 'Uh Oh. Looks like something went wrong.'
+};
+const DEVICE_LOCATION_PERMISSION = 'read::alexa:device:all:address';
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = `Hello, welcome to ${APP_NAME}. You can ask about device address by saying what is my location`;
+    const repromptText = 'What would you like to know?';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(repromptText)
+      .withSimpleCard(APP_NAME, speechText)
+      .getResponse();
+  },
+};
+
+const DeviceLocationIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'DeviceLocationIntent';
+  },
+  async handle(handlerInput) {
+    const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
+    try {
+      const { deviceId } = requestEnvelope.context.System.device;
+      const deviceAddressServiceClient = serviceClientFactory.getDeviceAddressServiceClient();
+      const address = await deviceAddressServiceClient.getFullAddress(deviceId);
+      let response;
+      if (address == undefined || (address.addressLine1 === null && address.stateOrRegion === null)) {
+        response = responseBuilder.speak(messages.NO_ADDRESS).getResponse();
+        return response;
+      } else {
+        const completeAddress = `${address.addressLine1}, ${address.stateOrRegion}, ${address.postalCode}`;
+        const response = `Your complete address is, ${completeAddress}`;
+        return handlerInput.responseBuilder
+            .speak(response)
+            .withSimpleCard(APP_NAME, response)
+            .getResponse();
+      }
+    } catch (error) {
+      console.log(JSON.stringify(error));
+      if (error.statusCode == 403) {
+        return responseBuilder
+        .speak(messages.NOTIFY_MISSING_PERMISSIONS)
+        .withAskForPermissionsConsentCard([DEVICE_LOCATION_PERMISSION])
+        .getResponse();
+      }
+      console.log(JSON.stringify(error));
+      const response = responseBuilder.speak(messages.ERROR).getResponse();
+      return response;
+    }
+  },
+};
+
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can ask for the address';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    DeviceLocationIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .withApiClient(new Alexa.DefaultApiClient())
+  .lambda();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Alexa Device Address API - Alexa Skill Template
+
+This is a simple template that shows how to request permissions and access device address in an Alexa skill that uses the [Alexa Skills Kit SDK 2.0 for Node.js](https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs).).
